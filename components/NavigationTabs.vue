a4db1fb6a8b4cd7f19098934ffb9401dd6084a6c
acrobid <116527022+acrobid@users.noreply.github.com>
2023-09-30 07:00:33 -0600

    working ionic tabs

--- components/NavigationTabs.vue
+++ components/NavigationTabs.vue
@@ -1,21 +1,21 @@
 <template>
   <ion-tab-bar>
-    <ion-tab-button tab="index" href="/index">
+    <ion-tab-button tab="tab1" href="/tabs/tab1">
       <Icon name="ant-design:schedule-outlined" size="2em" />
       <ion-label>Home</ion-label>
     </ion-tab-button>
 
-    <ion-tab-button tab="duty" href="/DutyLimits">
+    <ion-tab-button tab="tab2" href="/tabs/tab2">
       <Icon name="icon-park-outline:in-flight" size="2em" />
       <ion-label>Duty</ion-label>
     </ion-tab-button>
 
-    <ion-tab-button tab="fatigue" href="/FatigueAssessment">
+    <ion-tab-button tab="tab3" href="/tabs/tab3">
       <Icon name="mdi:sleep-schedule" size="2em" />
       <ion-label>Fatigue</ion-label>
     </ion-tab-button>
 
-    <ion-tab-button tab="links" href="/links">
+    <ion-tab-button tab="tab4" href="/tabs/tab4">
       <Icon name="mdi:search" size="2em" />
       <ion-label>Links</ion-label>
     </ion-tab-button>
---
+++ pages/tabs.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <ion-tabs>
+        <ion-router-outlet />
+
+        <NavigationTabs />
+      </ion-tabs>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
