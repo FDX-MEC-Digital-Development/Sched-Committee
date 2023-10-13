1a4f9d0ef37618fd3eacdcc484d89b108cd3e318
acrobid <116527022+acrobid@users.noreply.github.com>
2023-10-13 12:54:52 -0600

    feat: remove profile logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -30,64 +30,7 @@
                 </div>
               </div>
             </div>
-            <div class="hidden md:block">
-              <div class="ml-4 flex items-center md:ml-6">
-                <button
-                  type="button"
-                  class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span class="absolute -inset-1.5" />
-                  <span class="sr-only">View notifications</span>
-                  <Icon
-                    name="material-symbols:add-alert-rounded"
-                    class="h-6 w-6"
-                    aria-hidden="true"
-                  />
-                </button>
-                <!-- Profile dropdown -->
-                <HeadlessMenu as="div" class="relative ml-3">
-                  <div>
-                    <HeadlessMenuButton
-                      class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span class="absolute -inset-1.5" />
-                      <span class="sr-only">Open user menu</span>
-                      <img
-                        class="h-8 w-8 rounded-full"
-                        :src="user.imageUrl"
-                        alt=""
-                      >
-                    </HeadlessMenuButton>
-                  </div>
-                  <transition
-                    enter-active-class="transition ease-out duration-100"
-                    enter-from-class="transform opacity-0 scale-95"
-                    enter-to-class="transform opacity-100 scale-100"
-                    leave-active-class="transition ease-in duration-75"
-                    leave-from-class="transform opacity-100 scale-100"
-                    leave-to-class="transform opacity-0 scale-95"
-                  >
-                    <HeadlessMenuItems
-                      class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    >
-                      <HeadlessMenuItem
-                        v-for="item in userNavigation"
-                        :key="item.name"
-                        v-slot="{ active }"
-                      >
-                        <a
-                          :href="item.href"
-                          :class="[
-                            active ? 'bg-gray-100' : '',
-                            'block px-4 py-2 text-sm text-gray-700',
-                          ]"
-                        >{{ item.name }}</a>
-                      </HeadlessMenuItem>
-                    </HeadlessMenuItems>
-                  </transition>
-                </HeadlessMenu>
-              </div>
-            </div>
+
             <div class="-mr-2 flex md:hidden">
               <!-- Mobile menu button -->
               <HeadlessDisclosureButton
