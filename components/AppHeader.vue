4e5a62e8a783b11d94dae8f77eac1923b4ecfcf3
acrobid <116527022+acrobid@users.noreply.github.com>
2023-10-18 16:26:01 +0200

    feat: duty limit card formatting

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="min-h-full">
-    <div class="bg-indigo-600 pb-32">
-      <HeadlessDisclosure v-slot="{ open }" as="nav" class="border-b border-indigo-300 border-opacity-25 bg-indigo-600 lg:border-none">
+    <div class="bg-red-800 pb-32">
+      <HeadlessDisclosure v-slot="{ open }" as="nav" class="border-b border-red-300 border-opacity-25 bg-red-700 lg:border-none">
         <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-          <div class="relative flex h-16 items-center justify-between lg:border-b lg:border-indigo-400 lg:border-opacity-25">
+          <div class="relative flex h-16 items-center justify-between lg:border-b lg:border-red-400 lg:border-opacity-25">
             <div class="flex items-center px-2 lg:px-0">
               <div class="flex-shrink-0">
-                <img class="block h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300" alt="Your Company">
+                <img class="block h-8 w-8" src="./../assets/thumbnail_scheduling logo crop2.0.png" alt="Scheduling committee logo">
               </div>
               <div class="hidden lg:ml-10 lg:block">
                 <div class="flex space-x-4">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-white hover:bg-indigo-500 hover:bg-opacity-75', 'rounded-md py-2 px-3 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-red-700 text-white' : 'text-white hover:bg-red-500 hover:bg-opacity-75', 'rounded-md py-2 px-3 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
                 </div>
               </div>
             </div>
@@ -21,28 +21,19 @@
                   <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                     <Icon name="heroicons-solid:search" class="h-5 w-5" aria-hidden="true" />
                   </div>
-                  <input id="search" class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:text-sm sm:leading-6" placeholder="Search" type="search" name="search">
+                  <input id="search" class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-600 sm:text-sm sm:leading-6" placeholder="Search" type="search" name="search">
                 </div>
               </div>
             </div>
             <div class="flex lg:hidden">
               <!-- Mobile menu button -->
-              <HeadlessDisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-indigo-600 p-2 text-indigo-200 hover:bg-indigo-500 hover:bg-opacity-75 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
+              <HeadlessDisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-red-600 p-2 text-red-200 hover:bg-red-500 hover:bg-opacity-75 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-600">
                 <span class="absolute -inset-0.5" />
                 <span class="sr-only">Open main menu</span>
                 <Icon v-if="!open" name="heroicons-outline:menu" class="block h-6 w-6" aria-hidden="true" />
 
                 <Icon v-else name="heroicons-outline:x" class="block h-6 w-6" aria-hidden="true" />
               </HeadlessDisclosureButton>
-            </div>
-            <div class="hidden lg:ml-4 lg:block">
-              <div class="flex items-center">
-                <button type="button" class="relative flex-shrink-0 rounded-full bg-indigo-600 p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
-                  <span class="absolute -inset-1.5" />
-                  <span class="sr-only">View notifications</span>
-                  <Icon name="heroicons-outline:bell" class="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
             </div>
           </div>
         </div>
@@ -54,7 +45,7 @@
               :key="item.name"
               as="a"
               :href="item.href"
-              :class="[item.current ? 'bg-indigo-700 text-white' : 'text-white hover:bg-indigo-500 hover:bg-opacity-75', 'block rounded-md py-2 px-3 text-base font-medium']"
+              :class="[item.current ? 'bg-red-700 text-white' : 'text-white hover:bg-red-500 hover:bg-opacity-75', 'block rounded-md py-2 px-3 text-base font-medium']"
               :aria-current="item.current ? 'page' : undefined"
             >
               {{ item.name }}
--- pages/duty.vue
+++ pages/duty.vue
@@ -1,9 +1,15 @@
 <template>
   <main class="-mt-32">
     <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-      <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+      <div class="rounded-lg bg-grey px-5 py-6 shadow sm:px-6">
         <CardHeading title="Duty Limits" description="Use this tool to calulate scheduled, operational, and FAR duty limits." />
         <DutyLimitCard title="Results" stat="4" />
+        <DutyLimitCard title="Results" stat="4" />
+
+        <DutyLimitCard title="Results" stat="4" />
+
+        <CBAReference />
+        <DutyLimitDisclaimer />
       </div>
     </div>
   </main>
