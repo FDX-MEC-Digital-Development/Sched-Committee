e79fbee5e8edd60f35be7d140b5cedd3605c15f8
acrobid <116527022+acrobid@users.noreply.github.com>
2023-11-13 18:14:41 +0100

    feat: add safe area for iPhone notch

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <AppLayout><NuxtPage /></AppLayout>
+    <AppLayout class="safe-area-inset-bottom safe-area-inset-top safe-area-inset-left safe-area-inset-right">
+      <NuxtPage />
+    </AppLayout>
   </div>
 </template>
 
@@ -20,6 +22,28 @@ useHead({
 
 </script>
 
-<style>
+<style scoped>
+:root {
+  --safe-area-inset-top: env(safe-area-inset-top);
+  --safe-area-inset-bottom: env(safe-area-inset-bottom);
+  --safe-area-inset-left: env(safe-area-inset-left);
+  --safe-area-inset-right: env(safe-area-inset-right);
+}
+
+.safe-area-inset-top {
+  padding-top: var(--safe-area-inset-top);
+}
+
+.safe-area-inset-bottom {
+  padding-bottom: var(--safe-area-inset-bottom);
+}
+
+.safe-area-inset-left {
+  padding-left: var(--safe-area-inset-left);
+}
+
+.safe-area-inset-right {
+  padding-right: var(--safe-area-inset-right);
+}
 
 </style>
