5a18a870ce77d5833d82dea4afd2050e5b3c3bc3
acrobid <116527022+acrobid@users.noreply.github.com>
2023-11-13 09:46:36 +0100

    feat: Add separate component for time selection and update handleDatePickerUpdate function

--- components/DateTimePicker.vue
+++ components/DateTimePicker.vue
@@ -7,12 +7,26 @@
       date-picker
       timezone="utc"
       :dark="isDark"
-      time-picker-inline
+      :enable-time-picker="false"
       :clearable="false"
       :month-change-on-scroll="false"
-      @update:model-value="(event) => handleDatePickerUpdate(event)"
+      :teleport-center="true"
+      @update:model-value="(dateString: string) => handleDatePickerUpdate({dateString})"
     />
-
+    <VueDatePicker
+      time-picker
+      :model-value="hoursMinutes"
+      utc="preserve"
+      timezone="utc"
+      :dark="isDark"
+      :clearable="false"
+      :format="`HH:mm'Z'`"
+      @update:model-value="(timeObject) => handleDatePickerUpdate({timeObject})"
+    >
+      <template #input-icon>
+        <Icon name="i-heroicons-clock" class="w-auto  px-3" />
+      </template>
+    </VueDatePicker>
     <UFormGroup />
   </uformgroup>
 </template>
@@ -24,14 +38,18 @@ import { UTCDateMini } from '@date-fns/utc';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 
-defineProps({
+const props = defineProps({
   date: {
     type: Date,
     default: (new UTCDateMini()),
   },
 },
-
 );
+
+const hoursMinutes = computed(() => ({
+  hours: props.date.getHours(),
+  minutes: props.date.getMinutes(),
+}));
 
 const colorMode = useColorMode();
 const isDark = computed(() => colorMode.value === 'dark');
@@ -50,13 +68,24 @@ const emit = defineEmits(['update:date']);
   }
 } */
 
-function handleDatePickerUpdate (dateTimeString:string) {
-  console.log({ dateTimeString });
-  const [year, month, day, hour, minute] = dateTimeString.split(/[-T:]/).map(x => parseInt(x, 10));
-  const newDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
-  if (isValid(newDate)) {
-    console.log(`Updating dutyStartTimeZulu to ${newDate}`);
-    emit('update:date', newDate);
+function handleDatePickerUpdate ({ dateString, timeObject }: {dateString?: string | undefined, timeObject?: {hours: number, minutes: number, seconds: number} | undefined}) {
+  if (dateString === undefined && timeObject === undefined) {
+    return;
+  }
+  console.log({ dateString, timeObject });
+  if (dateString !== undefined) {
+    const [year, month, day, hour, minute] = dateString.split(/[-T:]/).map(x => parseInt(x, 10));
+    const newDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
+    if (isValid(newDate)) {
+      console.log(`Updating dutyStartTimeZulu to ${newDate}`);
+      emit('update:date', newDate);
+    }
+  } else if (timeObject !== undefined) {
+    const newDate = new UTCDateMini(Date.UTC(props.date.getFullYear(), props.date.getMonth(), props.date.getDate(), timeObject.hours, timeObject.minutes));
+    if (isValid(newDate)) {
+      console.log(`Updating dutyStartTimeZulu to ${newDate}`);
+      emit('update:date', newDate);
+    }
   }
 }
 
