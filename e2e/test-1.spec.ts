95f9d2b0beaf2347ea7dffc913ae41a4043ce9eb
acrobid <116527022+acrobid@users.noreply.github.com>
2023-11-23 15:06:04 +0100

    test: first e2e test

--- e2e/test-1.spec.ts
+++ e2e/test-1.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('/');
+  // expect to have text "ALPA Scheduling Committee" on the page
+  await page.goto('http://localhost:3000/');
+  await expect(page.getByRole('heading', { name: 'ALPA Scheduling Committee' })).toBeVisible();
+  await expect(page.locator('h1')).toContainText('ALPA Scheduling Committee');
+  await page.getByRole('navigation').getByRole('link', { name: 'Duty Limits' }).click();
+  await expect(page.locator('h3')).toContainText('Duty Limits');
 });
