6d719c353702c8e1d74614771bec923bbaa02841
acrobid <116527022+acrobid@users.noreply.github.com>
2023-11-28 12:55:33 +0100

    fix: fix pwa icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,17 +58,17 @@ export default defineNuxtConfig({
       theme_color: '#000000',
       icons: [
         {
-          src: 'pwa-192x192.png',
+          src: 'assets/pwa-192x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: 'pwa-512x512.png',
+          src: 'assets/pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
         },
         {
-          src: 'pwa-512x512.png',
+          src: 'assets/pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
