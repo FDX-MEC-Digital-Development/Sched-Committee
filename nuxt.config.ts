15c5b156558b93c3eba22ce45209c8655227a7ba
acrobid <116527022+acrobid@users.noreply.github.com>
2023-11-28 13:24:37 +0100

    fix: try "any" for 2 icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,6 +64,12 @@ export default defineNuxtConfig({
           type: 'image/png',
         },
         {
+          src: 'assets/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
           src: 'assets/pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
@@ -73,12 +79,6 @@ export default defineNuxtConfig({
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any',
-        },
-        {
-          src: 'assets/pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable',
         },
       ],
     },
