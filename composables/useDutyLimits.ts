22f9f74c91819136ad8f6e2f991383896bd6d7b4
acrobid <116527022+acrobid@users.noreply.github.com>
2024-05-12 22:53:40 +0200

    chore: license

--- composables/useDutyLimits.ts
+++ composables/useDutyLimits.ts
@@ -1,3 +1,18 @@
+// © 2024 Air Line Pilots Association, International.
+
+// This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { addMinutes, isValid } from 'date-fns';
 import { formatInTimeZone } from 'date-fns-tz';
 import { computed, toRef, toValue } from 'vue';
