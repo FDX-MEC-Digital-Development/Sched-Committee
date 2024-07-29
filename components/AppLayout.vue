0d192115df55ebaed2d28d96f2d658f630809d04
acrobid <116527022+acrobid@users.noreply.github.com>
2024-07-29 21:04:04 +0900

    initial

--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -77,6 +77,9 @@ const navigation = computed(() => ([
   { name: 'Home', href: '/', icon: 'heroicons:home', current: route.name === 'index' },
   { name: 'Duty Limits', href: '/dutyLimits', icon: 'heroicons:clock', current: route.name === 'dutyLimits' },
   { name: 'Fatigue', href: '/fatigue', icon: 'heroicons:bell-alert', current: route.name === 'fatigue' },
+  // rest
+  { name: 'Rest', href: '/rest', icon: 'heroicons:moon', current: route.name === 'rest' },
+
   // { name: 'Links', href: '#', icon: 'heroicons:link', current: false },
 
 ]));
