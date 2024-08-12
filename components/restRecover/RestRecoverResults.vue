3fb5cd8e019f8f6fe741c92aea2a2ff82a182345
acrobid <116527022+acrobid@users.noreply.github.com>
2024-08-12 09:23:07 +0900

    refactor: extract results

---
+++ components/restRecover/RestRecoverResults.vue
@@ -0,0 +1,67 @@
+<template>
+  <CardGrid>
+    <CardGridItem :key="JSON.stringify(results.cbaLink.value?.reference)" class="stagger-list ">
+      <StaggerList>
+        <ResultsCard title="Results" :link-title="results.cbaLink.value?.reference" :link-href="results.cbaLink.value?.link" class="result">
+          <template #default>
+            <div class="space-y-4">
+              <dl>
+                <dt class="flex self-center font-medium leading-6 text-gray-900  items-center  justify-between w-full ">
+                  <span class="sr-only">Scheduled</span>
+                  Scheduled
+                </dt>
+                <dd class="text-sm font-medium leading-6 text-gray-900  items-center flex justify-between w-full" :aria-label="`Scheduled rest`">
+                  <UBadge size="lg">
+                    {{ minutesToHours(results.restMinutesRequiredScheduled.value) }}
+                  </UBadge>
+                </dd>
+              </dl>
+              <dl v-if="results.restMinutesOperationallyReducableTo.value !== undefined">
+                <dt class="flex self-center font-medium leading-6 text-gray-900  items-center  justify-between w-full ">
+                  <span class="sr-only">Operational</span>
+                  Operational
+                </dt>
+                <dd class="text-sm font-medium leading-6 text-gray-900  items-center flex justify-between w-full" :aria-label="`operational rest`">
+                  <UBadge size="lg">
+                    {{ minutesToHours(results.restMinutesOperationallyReducableTo.value) }}
+                  </UBadge>
+                </dd>
+              </dl>
+            </div>
+          </template>
+          <template #notes>
+            <div v-if="notes.length > 0">
+              <dt class="flex-none">
+                <span class="sr-only">Notes</span>
+                <UIcon name="i-heroicons-shield-exclamation-solid" class="h-6 w-5 text-gray-400" aria-hidden="true" />
+              </dt>
+              <dd v-for="note in notes" :key="note" class="text-sm leading-6 text-gray-500">
+                {{ note }}
+              </dd>
+            </div>
+          </template>
+        </ResultsCard>{{ results }}
+      </StaggerList>
+    </CardGridItem>
+  </CardGrid>
+</template>
+
+<script lang="ts" setup>
+import { useRestRecover } from '~/composables/useRestRecover';
+
+defineProps<{
+  results: ReturnType<typeof useRestRecover>
+    notes: string[]
+}>();
+
+function minutesToHours (minutes: number): string {
+  const hours = Math.floor(minutes / 60);
+  const remainder = minutes % 60;
+  return `${hours}:${remainder.toString().padStart(2, '0')}`;
+}
+
+</script>
+
+<style>
+
+</style>
--- pages/RestRecover.vue
+++ pages/RestRecover.vue
@@ -4,54 +4,10 @@
       <RestRecoverForm v-model:options="options" v-model:duty-end-time-zulu="dutyEndTimeZulu" />
     </template>
     <template #button>
-      <UButton label="View Rest" class="result execute rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600" @click="isResultsVisible = true" />
+      <UButton label="View Rest" class="result execute rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600" @click="handleViewResults()" />
     </template>
     <template #results>
-      <CardGrid>
-        <CardGridItem :key="JSON.stringify(options)" class="stagger-list ">
-          <StaggerList>
-            <ResultsCard title="Results" :link-title="results.cbaLink.value?.reference" :link-href="results.cbaLink.value?.link">
-              <template #default>
-                <div class="space-y-4">
-                  <dl>
-                    <dt class="flex self-center font-medium leading-6 text-gray-900  items-center  justify-between w-full ">
-                      <span class="sr-only">Scheduled</span>
-                      Scheduled
-                    </dt>
-                    <dd class="text-sm font-medium leading-6 text-gray-900  items-center flex justify-between w-full" :aria-label="`Scheduled rest`">
-                      <UBadge size="lg">
-                        {{ minutesToHours(results.restMinutesRequiredScheduled.value) }}
-                      </UBadge>
-                    </dd>
-                  </dl>
-                  <dl v-if="results.restMinutesOperationallyReducableTo.value !== undefined">
-                    <dt class="flex self-center font-medium leading-6 text-gray-900  items-center  justify-between w-full ">
-                      <span class="sr-only">Operational</span>
-                      Operational
-                    </dt>
-                    <dd class="text-sm font-medium leading-6 text-gray-900  items-center flex justify-between w-full" :aria-label="`operational rest`">
-                      <UBadge size="lg">
-                        {{ minutesToHours(results.restMinutesOperationallyReducableTo.value) }}
-                      </UBadge>
-                    </dd>
-                  </dl>
-                </div>
-              </template>
-              <template #notes>
-                <div v-if="notes.length > 0">
-                  <dt class="flex-none">
-                    <span class="sr-only">Notes</span>
-                    <UIcon name="i-heroicons-shield-exclamation-solid" class="h-6 w-5 text-gray-400" aria-hidden="true" />
-                  </dt>
-                  <dd v-for="note in notes" :key="note" class="text-sm leading-6 text-gray-500">
-                    {{ note }}
-                  </dd>
-                </div>
-              </template>
-            </ResultsCard>{{ results }}
-          </StaggerList>
-        </CardGridItem>
-      </CardGrid>
+      <RestRecoverResults :results :notes />
     </template>
   </GenericToolPage>
 </template>
@@ -93,10 +49,20 @@ const notes = computed(() => results.notes.value ? results.notes.value : []);
 // eslint-disable-next-line no-console
 watchEffect(() => console.log({ results }));
 
-function minutesToHours (minutes: number): string {
-  const hours = Math.floor(minutes / 60);
-  const remainder = minutes % 60;
-  return `${hours}:${remainder.toString().padStart(2, '0')}`;
+async function handleViewResults () {
+  isResultsVisible.value = true;
+  await nextTick();
+
+  const resultElement = document.querySelector('.result');
+  resultElement?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+/*   $anime({
+    targets: '.result',
+    translateX: [50, 0],
+    opacity: [0, 1],
+    easing: 'easeInQuad',
+    duration: 1000,
+  }); */
 }
 
 </script>
