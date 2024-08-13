44d2cc144bcafc63b85b41d2c00a1ff2fef6a19f
acrobid <116527022+acrobid@users.noreply.github.com>
2024-08-13 13:58:19 +0900

    change domestic options to radio group

--- components/OptionsCheckbox.vue
+++ components/OptionsCheckbox.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="mt-6 space-y-6">
-    <div class="relative flex gap-x-3">
-      <div class="flex h-6 items-center">
+    <div class="container">
+      <div v-if="label" class="label">
+        <label for="comments" class="font-medium text-gray-900 dark:text-white">{{ label }}</label>
+      </div>
+      <div class="checkbox">
         <UCheckbox :model-value="model" @update:model-value="(event) => model = event" />
       </div>
-      <div class="text-sm leading-6">
-        <label for="comments" class="font-medium text-gray-900 dark:text-white">{{ label }}</label>
+      <div class="caption text-sm leading-6">
         <p class="text-gray-500">
           {{ caption }}
         </p>
@@ -18,11 +20,28 @@
 
 const model = defineModel<boolean>();
 defineProps<{
-  label: string,
+  label?: string,
   caption: string
 }>();
 </script>
 
 <style>
+.container {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: 0.75rem 0.5rem; /* Adjust the gap as needed */
+  align-items: center;
+}
 
+.label {
+  grid-column: span 2;
+}
+
+.checkbox {
+  grid-column: 1;
+}
+
+.caption {
+  grid-column: 2;
+}
 </style>
--- components/restRecover/RestRecoverDomesticOptions.vue
+++ components/restRecover/RestRecoverDomesticOptions.vue
@@ -2,13 +2,7 @@
   <form>
     <div class="max-w-2xl space-y-10 md:col-span-2">
       <fieldset>
-        <OptionsCheckbox v-model="domesticOptions.afterExceed8BlockHoursIn24Hours" label="High Block trips" caption="Have you just exceeded 8 block hours in the last 24?" />
-        <OptionsCheckbox v-model="domesticOptions.priorToExceed8BlockHoursIn24Hours" label="High Block trips" caption="Are you about to exceed 8 block hours in the next 24?" />
-        <OptionsCheckbox v-model="domesticOptions.exception12C2dPriorToExceeding735ActualBlockHours" label="12C2d Exception" caption="Are you operating under the 12C2d exception before exceeding 7:35 actual block hours?" />
-        <OptionsCheckbox v-model="domesticOptions.exception12C2dAfterExceeding735ActualBlockHours" label="12C2d Exception" caption="Are you operating under the 12C2d exception after exceeding 7:35 actual block hours?" />
-
-        <OptionsCheckbox v-model="domesticOptions.hotelStbyScenario" label="Hotel Standby Scenario" caption="Are you in a hotel standby scenario?" />
-        <OptionsCheckbox v-model="domesticOptions.operatingInCriticalPeriod" label="Operating in Critical Period" caption="Are you operating in a critical period?" />
+        <URadioGroup :options="radioOptions" :model-value="currentOption" :ui="{fieldset: 'space-y-2'}" @update:model-value="updateModel" />
       </fieldset>
     </div>
   </form>
@@ -19,6 +13,15 @@ import type { DomesticRestOptions } from '~/sched-committee-types';
 
 const domesticOptions = defineModel<DomesticRestOptions>('domesticOptions', { required: true });
 
+const currentOption = computed(() => {
+  for (const key in domesticOptions.value) {
+    if (domesticOptions.value[key as keyof typeof domesticOptions.value]) {
+      return key;
+    }
+  }
+  return 'none';
+});
+
 /* export interface DomesticRestOptions {
   operatingInCriticalPeriod: boolean, // must also receive less than 11 hours of rest operationally for this condition to be true
   hotelStbyScenario: boolean,
@@ -27,8 +30,40 @@ const domesticOptions = defineModel<DomesticRestOptions>('domesticOptions', { re
   exception12C2d: boolean,
 } */
 
+const selectedOption = ref<string>('none');
+
+function updateModel (value: string) {
+  const newOptions = { ...domesticOptions.value };
+
+  if (value === 'none') {
+    // Set all options to false
+    Object.keys(newOptions).forEach((key) => {
+      newOptions[key as keyof typeof newOptions] = false;
+    });
+  } else {
+    // Update options based on the selected value
+    Object.keys(newOptions).forEach((key) => {
+      newOptions[key as keyof typeof newOptions] = key === value;
+    });
+    selectedOption.value = value;
+  }
+
+  domesticOptions.value = newOptions;
+}
+
+// [{value: 'afterExceed8BlockHoursIn24Hours', label: 'High Block trips', help: 'Have you just exceeded 8 block hours in the last 24?'}]
+const radioOptions = [
+  { value: 'none', label: 'None', help: 'No options apply.' },
+  { value: 'afterExceed8BlockHoursIn24Hours', label: 'High Block trips', help: 'Have you just exceeded 8 block hours in the last 24?' },
+  { value: 'priorToExceed8BlockHoursIn24Hours', label: 'High Block trips', help: 'Are you about to exceed 8 block hours in the next 24?' },
+  { value: 'exception12C2dPriorToExceeding735ActualBlockHours', label: '12C2d Exception', help: 'Are you operating under the 12C2d exception before exceeding 7:35 actual block hours?' },
+  { value: 'exception12C2dAfterExceeding735ActualBlockHours', label: '12C2d Exception', help: 'Are you operating under the 12C2d exception after exceeding 7:35 actual block hours?' },
+  { value: 'hotelStbyScenario', label: 'Hotel Standby Scenario', help: 'Are you in a hotel standby scenario?' },
+  { value: 'operatingInCriticalPeriod', label: 'Operating in Critical Period', help: 'Are you operating in a critical period?' },
+];
+
 </script>
 
-<style>
+<style scoped>
 
 </style>
